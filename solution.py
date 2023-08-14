8c4a7a746c6139ad46e6339a334bc09d5fdbe566
pelsingunduz <pelsingunduz@icloud.com>
2023-08-14 14:48:09 +0300

    second commit

--- solution.py
+++ solution.py
@@ -16,45 +16,40 @@ def main():
     df = pd.read_csv('sales.csv')
     df['date'] = pd.to_datetime(df['date'])
 
-    # Ürün bazında hareketli ortalama (MA7_P) ve gecikmeli değer (LAG7_P) hesaplama
-    df['MA7_P'] = df.groupby('product')['quantity'].transform(lambda x: x.rolling(window=7).mean())
-    df['LAG7_P'] = df.groupby('product')['quantity'].transform(lambda x: x.shift(7))
+    # Belirli bir tarih aralığına göre satış verilerini filtreleme
+    filtered_df = df[(df['date'] >= min_date) & (df['date'] <= max_date)]
 
-    # Marka verilerini okuma
+    #Marka ve mağaza verilerini okuma
     brands_df = pd.read_csv('brand.csv')
-    
+    stores_df = pd.read_csv('store.csv')
+
     # Satış verileri ile marka verilerini "product" sütunu üzerinden birleştirme
-    merged_brand = df.merge(brands_df, left_on='product', right_on='id')
-    
-    # Mağaza, marka ve tarih bazında toplam satış miktarlarını hesaplama
+    merged_brand = filtered_df.merge(brands_df, left_on='product', right_on='id')
+
+    # Mağaza verileri ile mağaza verilerini "store" sütunu üzerinden birleştirme
+    merged_store = filtered_df.merge(stores_df, left_on='store', right_on='id')
+
+    # Mağaza, marka ve tarih bazında toplam satış miktarlarını hesaplama 
     brand_agg = merged_brand.groupby(['store', 'id', 'date'])['quantity'].sum().reset_index()
-    
-    # Mağaza ve marka bazında hareketli ortalama (MA7_B) ve gecikmeli değer (LAG7_B) hesaplama
-    brand_agg['MA7_B'] = brand_agg.groupby(['store', 'id'])['quantity'].transform(lambda x: x.rolling(window=7).mean())
+    brand_agg = brand_agg[(brand_agg['date'] >= min_date) & (brand_agg['date'] <= max_date)]
+
+    # Mağaza ve marka bazında hareketli ortalama (MA7_B) ve gecikmeli değer (LAG7_B) hesaplama 
+    brand_agg['MA7_B'] = brand_agg.groupby(['store', 'id'])['quantity'].transform(lambda x: x.rolling(window=7, min_periods=1).mean())
     brand_agg['LAG7_B'] = brand_agg.groupby(['store', 'id'])['quantity'].transform(lambda x: x.shift(7))
     brand_agg['date'] = pd.to_datetime(brand_agg['date'])
 
-    # Mağaza verilerini okuma
-    stores_df = pd.read_csv('store.csv')
-    
-    # Satış verileri ile mağaza verilerini "store" sütunu üzerinden birleştirme
-    merged_store = df.merge(stores_df, left_on='store', right_on='id')
-    
-    # Mağaza bazında toplam satış miktarlarını hesaplama
+    # Mağaza bazında toplam satış miktarlarını hesaplama 
     store_agg = merged_store.groupby(['store', 'date'])['quantity'].sum().reset_index()
-    
-    # Mağaza bazında hareketli ortalama (MA7_S) ve gecikmeli değer (LAG7_S) hesaplama
-    store_agg['MA7_S'] = store_agg.groupby('store')['quantity'].transform(lambda x: x.rolling(window=7).mean())
+    store_agg = store_agg[(store_agg['date'] >= min_date) & (store_agg['date'] <= max_date)]
+
+    # Mağaza bazında hareketli ortalama (MA7_S) ve gecikmeli değer (LAG7_S) hesaplama 
+    store_agg['MA7_S'] = store_agg.groupby('store')['quantity'].transform(lambda x: x.rolling(window=7, min_periods=1).mean())
     store_agg['LAG7_S'] = store_agg.groupby('store')['quantity'].transform(lambda x: x.shift(7))
     store_agg['date'] = pd.to_datetime(store_agg['date'])
 
     # Sonuç veri çerçevelerini birleştirme
-    result = df.merge(brand_agg, on=['store', 'date'], how='left', suffixes=('', '_brand'))
+    result = filtered_df.merge(brand_agg, on=['store', 'date'], how='left', suffixes=('', '_brand'))
     result = result.merge(store_agg, on=['store', 'date'], how='left', suffixes=('', '_store'))
-
-    # Sonuç veri çerçevesini seçme ve sütun adlarını düzenleme
-    result = result[['product', 'store', 'id', 'date', 'quantity', 'MA7_P', 'LAG7_P', 'quantity_brand', 'MA7_B', 'LAG7_B', 'quantity_store', 'MA7_S', 'LAG7_S']]
-    result.columns = ['product_id', 'store_id', 'brand_id', 'date', 'sales_product', 'MA7_P', 'LAG7_P', 'sales_brand', 'MA7_B', 'LAG7_B', 'sales_store', 'MA7_S', 'LAG7_S']
 
     # Sonuçları CSV dosyasına yazma
     result.to_csv('features.csv', index=False)
